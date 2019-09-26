3b875439bb4bf9d1174fa060258dd10f97358b82
j84077200345 <j84077200345@gmail.com>
2019-09-26 13:36:48 +0800

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var firebaseAdminDB = require('../connections/firebase_admin');
+
+firebaseAdminDB.ref('any').once('value', function(snapshot) {
+  console.log(snapshot.val());
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/post', function(req, res, next) {
+  res.render('post', { title: 'Express' });
+});
+
+router.get('/dashboard/info', function(req, res, next) {
+  res.render('dashboard/info', { title: 'Express' });
+});
+
+router.get('/dashboard/signup', function(req, res, next) {
+  res.render('dashboard/signup', { title: 'Express' });
 });
 
 module.exports = router;
